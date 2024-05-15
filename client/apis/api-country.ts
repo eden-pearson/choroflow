67996f4056fbf2e835e1e76a129e41725f3cb029
Eden Pearson <pearsoneden@gmail.com>
2024-05-15 12:13:47 +1200

    fix port

--- client/apis/api-country.ts
+++ client/apis/api-country.ts
@@ -1,16 +1,18 @@
 import request from 'superagent'
-// TODO: add model
+import { Invention } from '../../models/Inventions'
+import type { Person } from '../../models/People'
+import type { Event } from '../../models/Events'
 
 export async function getNewZealandInventionsData() {
   const response = await request.get('/api/v1/newZealand/inventions')
-  return response.body
+  return response.body as Invention[]
 }
 
 export async function getNewZealandEventsData() {
   const response = await request.get('/api/v1/newZealand/events')
-  return response.body
+  return response.body as Person[]
 }
 export async function getNewZealandPeopleData() {
   const response = await request.get('/api/v1/newZealand/people')
-  return response.body
+  return response.body as Event[]
 }
