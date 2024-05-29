54b666876c219ff187850d6a5aabfaf030e93fb5
Eden Pearson <pearsoneden@gmail.com>
2024-05-29 18:41:30 +1200

    deployment files

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer>
       <p className="bg-black text-white fot-sans uppercase py-8 text-center">
-        Developed by the Good Times Guild, 2023. All rights reserved.
+        All rights reserved.
       </p>
     </footer>
   )
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,9 +2,6 @@ function Header() {
   return (
     <header className=" font-sans px-7 w-screen bg-zinc-900 mb-6 mt-6 h-14 flex items-center justify-between text-white">
       <h1 className="text-6xl font-bold ">Chronoflow</h1>
-      <p className="  fot-sans uppercase py-8 text-center">
-        Developed by the Good Times Guild, 2023. All rights reserved.
-      </p>
     </header>
   )
 }
